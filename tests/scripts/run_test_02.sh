4ad764ebde9b396f5b1ed42c5014f01a35e4c97e
Julian Thomas Mohr <julian.mohr@tron-mainz.de>
2023-07-10 11:49:30 +0200

    Use SARS-COV2 reference and sequencing data to fix GitLab CI issue

--- tests/scripts/run_test_02.sh
+++ tests/scripts/run_test_02.sh
@@ -16,7 +16,8 @@ input=`pwd`"/tests/output/${test_id}/input_${test_id}.tsv"
 output=`pwd`"/tests/output/${test_id}"
 # reference=`pwd`"/tests/data/current/Homo_sapiens_assembly38.chr6_22.fasta.gz"
 # reference=`pwd`"/tests/data/old/ucsc.hg19.minimal.fasta"
-reference=`pwd`"/tests/test-datasets/data/genomics/homo_sapiens/genome/chr21/sequence/genome.fasta"
+# reference=`pwd`"/tests/test-datasets/data/genomics/homo_sapiens/genome/chr21/sequence/genome.fasta"
+reference=`pwd`"/tests/test-datasets/data/genomics/sarscov2/genome/genome.fasta"
 # intervals=`pwd`"/tests/data/current/Exome-Agilent_V6.hg38.chr6_chr22.bed"
 # intervals=`pwd`"/tests/data/old/minimal_intervals.bed"
 intervals=`pwd`"/tests/test-datasets/data/genomics/homo_sapiens/genome/chr21/sequence/multi_intervals.bed"
@@ -32,7 +33,8 @@ mkdir -p $output
 
 # echo -e "${sample_id_tum}\t"`pwd`"/tests/data/current/tumor_WES.downsampled_0001.bam\t"`pwd`"/tests/data/current/normal_WES.downsampled_0001.bam" > $input
 # echo -e "${sample_id_tum}\t"`pwd`"/tests/data/old/TESTX_S1_L001_TUM1.bam\t"`pwd`"/tests/data/old/TESTX_S1_L001_NOR1.bam" > $input
-echo -e "${sample_id_tum}\t"`pwd`"/tests/test-datasets/data/genomics/homo_sapiens/illumina/bam/test.paired_end.markduplicates.sorted.bam\t"`pwd`"/tests/test-datasets/data/genomics/homo_sapiens/illumina/bam/test2.paired_end.markduplicates.sorted.bam" > $input
+# echo -e "${sample_id_tum}\t"`pwd`"/tests/test-datasets/data/genomics/homo_sapiens/illumina/bam/test.paired_end.markduplicates.sorted.bam\t"`pwd`"/tests/test-datasets/data/genomics/homo_sapiens/illumina/bam/test2.paired_end.markduplicates.sorted.bam" > $input
+echo -e "${sample_id_tum}\t"`pwd`"/tests/test-datasets/data/genomics/sarscov2/illumina/bam/test.paired_end.sorted.bam\t"`pwd`"/tests/test-datasets/data/genomics/sarscov2/illumina/bam/test.single_end.sorted.bam" > $input
 
 echo "Success: Input file created"
 
